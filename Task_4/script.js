6b1a1460f118dc498ea2aee8b22a0c62c4ddb6b4
ShemetViktoriia <shemetvic@ukr.net>
2019-12-17 03:34:41 +0200

    Task_4 was done

---
+++ Task_4/script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const expression = {
+    operator: getOperator(),
+    leftOperand: getOperand('Enter left operand'),
+    rightOperand: getOperand('Enter right operand'),
+
+    toString() {
+        return `${this.leftOperand}` +
+            `${this.operator}` +
+            `${this.rightOperand > 0 ? this.rightOperand : '(' + this.rightOperand + ')'} =` +
+            `${calculateResult(this.operator, this.leftOperand, this.rightOperand)}`;
+    }
+};
+
+alert(`${expression}`);
+
+function getOperator() {
+    const operators = ['+', '-', '*', '/', '%'];
+    let userOperator;
+    do {
+        userOperator = prompt('Enter one of the operators +, -, *, /, %');
+    } while (operators.indexOf(userOperator) == -1);
+    return userOperator;
+}
+
+function getOperand(promptText) {
+    let userInput;
+    do {
+        userInput = prompt(promptText);
+    } while (isNaN(userInput) || userInput === '');
+    return Number(userInput);
+}
+
+function calculateResult(operator, leftOperand, rightOperand) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = calculateSum(leftOperand, rightOperand);
+            break;
+        case '-':
+            result = calculateDif(leftOperand, rightOperand);
+            break;
+        case '*':
+            result = calculateMult(leftOperand, rightOperand);
+            break;
+        case '/':
+            result = calculateDiv(leftOperand, rightOperand);
+            break;
+        case '%':
+            result = calculateRemOfDiv(leftOperand, rightOperand);
+            break;
+    }
+    return result;
+}
+
+function calculateSum(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
+}
+
+function calculateDif(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
+}
+
+function calculateMult(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
+
+function calculateDiv(leftOperand, rightOperand) {
+    return leftOperand / rightOperand;
+}
+
+function calculateRemOfDiv(leftOperand, rightOperand) {
+    return leftOperand % rightOperand;
+}
