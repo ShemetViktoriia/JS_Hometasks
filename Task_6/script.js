ec93a5cdb69f7c330bb24fe6648043d47c1f57d0
ShemetViktoriia <shemetvic@ukr.net>
2020-01-05 23:49:59 +0200

    Changes

--- Task_6/script.js
+++ Task_6/script.js
@@ -11,31 +11,27 @@ do {
 } while (arrayOperators.indexOf(operator) == -1);
 
 do {
-    num = prompt('Enter number of operands');
-} while (num.trim() === '' || !Number.isInteger(+num) || +num < 1 || +num > 4);
-
-num = Number(num);
+    num = +prompt('Enter number of operands');
+} while (isNaN(num) || (num % 1) || num < 2 || num > 4);
 
 for (let i = 0; i < num; i++) {
-    let operand;
     do {
-        operand = prompt(`Enter ${i + 1} operand`);
-    } while (isNaN(operand) || operand.trim() === '');
-    operands[i] = Number(operand);
+        operands[i] = +prompt(`Enter ${i + 1} operand`);
+    } while (isNaN(operands[i]));
 }
 
 switch (operator) {
     case '+':
-        result = operands.reduce((sum, curr) => sum + curr, 0);
+        result = operands.reduce((sum, curr) => sum + curr);
         break;
     case '-':
-        result = operands.reduce((sub, curr) => sub - curr, 2*operands[0]);
+        result = operands.reduce((sub, curr) => sub - curr);
         break;
     case '*':
-        result = operands.reduce((mult, curr) => mult * curr, 1);
+        result = operands.reduce((mult, curr) => mult * curr);
         break;
     case '/':
-        result = operands.reduce((div, curr) => div / curr, operands[0]*operands[0]);
+        result = operands.reduce((div, curr) => div / curr);
         break;
 }
 
