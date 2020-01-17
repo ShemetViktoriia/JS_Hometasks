84b128cbab594080e2ff26fde690da6380adbad0
ShemetViktoriia <shemetvic@ukr.net>
2020-01-17 08:48:02 +0200

    Rework (code improvement)

--- Task_8/script.js
+++ Task_8/script.js
@@ -1,76 +1,53 @@
 'use strict';
 
-const ENTER_KEY_CODE = 13;
-const DEFAULT_CARD_CLASS = 'bg-warning';
-const CHANGE_CARD_CLASS = 'bg-success';
-
+const taskFormElement = document.querySelector('#addTaskForm');
+const taskListElement = document.querySelector('#taskList');
 const taskDescElement = document.querySelector('#taskDesc');
-const taskListElement = document.querySelector('#taskTemplate');
-const taskTemplate = document.querySelector('#taskTemplate').innerHTML;
 const valMessageElement = document.querySelector('#valMessage');
+const taskTemplate = document.querySelector('#taskTemplate').innerHTML;
 
-document.querySelector('#taskAdd').addEventListener('click', onTaskAddBtnClick);
+document.querySelector('#addTaskForm').addEventListener('submit', onTaskAddFormSubmit);
+taskListElement.addEventListener('click', onTaskListClick);
 
-taskDescElement.addEventListener('keydown', e => {
-    if (e.keyCode === ENTER_KEY_CODE) {
-        onTaskAddBtnClick(e);
-    }
-});
-
-function onTaskAddBtnClick(e) {
+function onTaskAddFormSubmit(e) {
     e.preventDefault();
     const description = getTaskDesc();
-    if (description === '') {
+    if (!isValid(description)) {
         showValMessage();
         return;
     }
-    generateNewTaskList(description);
+    generateNewTask(description);
     clearElements();
 }
 
-document.querySelector('#taskTemplate').addEventListener(
-    'click',
-    (e) => {
-        if (e.target.classList.length===0) return;
-
-        switchTaskCardColor(e.target.parentNode);
-    },
-    false
-);
-
-function switchTaskDivColor(element) {
-    let elemClassList = element.classList;
-
-    if(elemClassList.contains(DEFAULT_CARD_CLASS)){
-        elemClassList.toggle(DEFAULT_CARD_CLASS);
-        elemClassList.toggle(CHANGE_CARD_CLASS, true);
-    }
-    else if(elemClassList.contains(CHANGE_CARD_CLASS)){
-    elemClassList.toggle(CHANGE_CARD_CLASS);
-    elemClassList.toggle(DEFAULT_CARD_CLASS, true);
+function onTaskListClick(e) {
+    if (e.target.classList.contains('card-body')) {
+        switchTaskCardColor(e.target);
     }
 }
 
-function clearElements() {
-    taskDescElement.value = '';
-    valMessageElement.innerHTML = '';
+function switchTaskCardColor(element) {
+    element.classList.toggle('marked');
+}
+
+function generateNewTask(description) {
+    const htmlTemplate = taskTemplate.replace('{{desc}}', description);
+    taskListElement.innerHTML += htmlTemplate;
 }
 
 function getTaskDesc() {
     return taskDescElement.value;
 }
 
+function isValid(description) {
+    return description !== '';
+}
+
 function showValMessage() {
     valMessageElement.textContent = 'Description is required!';
 }
 
-function generateNewTaskList(description) {
-    const template = generateTaskDiv(description);
-    taskListElement.innerHTML += template;
-}
-
-function generateTaskDiv(description) {
-    return taskTemplate
-        .replace('{{desc}}', description)
-        .replace(' hidden=""','');
+function clearElements() {
+    taskFormElement.reset();
+    valMessageElement.innerHTML = '';
 }
